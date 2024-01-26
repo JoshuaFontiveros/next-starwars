1ae34021d3ef95ec428f7fa549ed088e8e75440c
Joshua <fontiveros.joshua@gmail.com>
2024-01-26 12:23:45 +0800

    Feat: film types integration to inputSearchComboBox

--- app/_components/InputSearchCombobox/index.tsx
+++ app/_components/InputSearchCombobox/index.tsx
@@ -2,48 +2,31 @@
 
 import { useMemo, useState } from 'react';
 import { isEmpty } from 'lodash';
-
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { FunctionComponent } from 'react';
-import { Input } from 'postcss';
 import { SearchBar } from '..';
-
-const frameworks = [
-  {
-    value: 'next.js',
-    label: 'Next.js',
-  },
-  {
-    value: 'sveltekit',
-    label: 'SvelteKit',
-  },
-  {
-    value: 'nuxt.js',
-    label: 'Nuxt.js',
-  },
-  {
-    value: 'remix',
-    label: 'Remix',
-  },
-  {
-    value: 'astro',
-    label: 'Astro',
-  },
-];
+import { Film } from '@/types/starWarTypes';
 
 interface InputSearchComboboxProps {
-  data: any[];
+  data: Film[];
 }
 
-const InputSearchCombobox: FunctionComponent<InputSearchComboboxProps> = (): React.ReactElement => {
+const InputSearchCombobox: FunctionComponent<InputSearchComboboxProps> = ({
+  data,
+}): React.ReactElement => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState('');
 
   const filteredValue =
     value === ''
-      ? frameworks
-      : frameworks.filter((framework) => {
-          return framework.value.toLowerCase().includes(value.toLowerCase());
+      ? data
+      : data.filter((data) => {
+          const searchValue = value.toLowerCase();
+          return (
+            data.title.toLowerCase().includes(searchValue) ||
+            data.episode_id.toString().includes(searchValue) ||
+            data.director.toLowerCase().includes(searchValue) ||
+            data.characters.some((character) => character.toLowerCase().includes(searchValue))
+          );
         });
 
   return (
@@ -53,9 +36,11 @@ const InputSearchCombobox: FunctionComponent<InputSearchComboboxProps> = (): Rea
         <div className='absolute h-52 mt-10 w-full z-[999]'>
           <div className='absolute bottom-0 shadow-lg border mt-2 rounded-md h-52 flex flex-col py-2 px-4 bg-white w-full'>
             {isEmpty(filteredValue) ? (
-              <span className='text-gray-500'>No results found</span>
+              <span className='text-gray-500'>No results foun</span>
             ) : (
-              filteredValue.map((framework) => <span key={framework.label}>{framework.label}</span>)
+              filteredValue.map((framework) => (
+                <span key={framework.episode_id}>{framework.title}</span>
+              ))
             )}
           </div>
         </div>
